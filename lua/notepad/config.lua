4e2c49d8df977758b00c816282081f12ca08dcc3
Carlos Calla <calla.alarcon@gmail.com>
2026-01-01 22:37:06 +0100

    Add opts validations

--- lua/notepad/config.lua
+++ lua/notepad/config.lua
@@ -9,9 +9,39 @@ M._opts = {
 }
 
 --- Set configuration from init module
----@param opts table Configuration options
+---@param opts? table Configuration options
 M.set_opts = function(opts)
-  M._opts = vim.tbl_deep_extend('force', M._opts, opts or {})
+  opts = opts or {}
+
+  -- Validate position
+  if opts.position then
+    local valid_positions = { top = true, bottom = true, left = true, right = true }
+    if not valid_positions[opts.position] then
+      vim.notify(
+        string.format("Invalid position '%s'. Using default 'bottom'. Valid options: top, bottom, left, right", opts.position),
+        vim.log.levels.WARN,
+        { title = 'Notepad' }
+      )
+      opts.position = nil -- Use default
+    end
+  end
+
+  -- Validate split_size
+  if opts.split_size ~= nil then
+    if type(opts.split_size) ~= 'number' then
+      vim.notify(
+        string.format("Invalid split_size type '%s'. Must be a number. Using default.", type(opts.split_size)),
+        vim.log.levels.WARN,
+        { title = 'Notepad' }
+      )
+      opts.split_size = nil -- Use default
+    elseif opts.split_size <= 0 then
+      vim.notify('Invalid split_size value. Must be greater than 0. Using default.', vim.log.levels.WARN, { title = 'Notepad' })
+      opts.split_size = nil -- Use default
+    end
+  end
+
+  M._opts = vim.tbl_deep_extend('force', M._opts, opts)
 end
 
 return M
